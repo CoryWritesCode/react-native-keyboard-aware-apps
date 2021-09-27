f26a64245ae6ef77603c46dad9829b9ed3b6a7cf
@corywritescode <corycunningham2010@gmail.com>
2021-09-27 12:43:44 -0400

    initial commit

---
+++ Post65/App.js
@@ -0,0 +1,143 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useRef} from 'react';
+import type {Node} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import {KeyboardAwareScrollView} from './react-native-keyboard-aware-scroll-view';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const scrollViewRef = useRef();
+  const textInputRef1 = useRef();
+  const textInputRef2 = useRef();
+  const textInputRef3 = useRef();
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const onPressScrollToEnd = () => scrollViewRef.current.scrollToEnd();
+  const onPressScrollToPosition = () =>
+    scrollViewRef.current.scrollToPosition(500, 500);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <KeyboardAwareScrollView
+        ref={scrollViewRef}
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <View style={styles.button}>
+            <Button
+              style={styles.button}
+              onPress={onPressScrollToEnd}
+              title={'Push me to scrollToEnd()'}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              style={styles.button}
+              onPress={onPressScrollToPosition}
+              title={'Push me to scrollToPosition(500,500)'}
+            />
+          </View>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  button: {
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+});
+
+export default App;
